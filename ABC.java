e1d114719cc5729409101e9c89d2704314158b4a
Nandhini211089 <nandhinikandasamy40@gmail.com>
2021-03-25 11:31:50 +0800

    updated file

---
+++ ABC.java
@@ -0,0 +1,25 @@
+class ABC
+{
+public static void main(String[] args)
+{
+byte by  = 12;
+short sh = 140;
+int i = 150;
+long l = 1000;
+float f = 6.8f;
+double d = 6.124;
+char ch = 'k';
+boolean graduate = true;
+String name = "Nandhini";
+System.out.println(by); 
+System.out.println(sh);
+System.out.println(i);
+System.out.println(l);
+System.out.println(f);
+System.out.println(d);
+System.out.println(ch);
+System.out.println(graduate);
+System.out.println(name);
+
+}
+}
